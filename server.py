f6bdc4756ccf9916c567f43417afa2669554e4c8
Cristian Pilco Miranda <cdpilco@gamma19.aulas.gsyc.urjc.es>
2016-11-21 09:56:35 +0100

    realizado captura correctamente

--- server.py
+++ server.py
@@ -25,10 +25,11 @@ class ServerHandler(socketserver.DatagramRequestHandler):
             if METODO == 'ACK':
                 # aEjecutar es un string con lo que se ha de ejecutar en la
                 # shell
-                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + fichero_audio
+                aEjecutar = 'mp32rtp -i '+ IP + ' -p 23032 < ' + fichero_audio
                 print("Vamos a ejecutar", aEjecutar)
                 os.system(aEjecutar)
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            else
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         elif METODO not in METODOS:
             self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
         else:
